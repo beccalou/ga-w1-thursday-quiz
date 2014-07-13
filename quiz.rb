6b46eb646e61f831a6047eece07d513ab0823a8f
Becky Bell <rlbell@alum.bu.edu>
2014-07-13 15:22:24 -0400

    corrected function to return array of natural numbers below 'max_num' that are multiples of 'multiple'

--- quiz.rb
+++ quiz.rb
@@ -1,11 +1,13 @@
 
 def multiples (multiple, max_num)
   numbers = Array(1..max_num)
-  multiples_of_multiple = numbers.select { |number| multiple % number == 0 }
+  multiples_of_multiple = numbers.select { |number| multiple * number < max_num}.map { |number| number * multiple }
   p multiples_of_multiple
-  
 end
 
-multiples(12, 6)
+ multiples(3,10)
+ multiples(2,8)
+ multiples(5,15)
+
 
 
